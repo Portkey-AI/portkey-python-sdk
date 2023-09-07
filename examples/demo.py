dd0a1a4a058dea60404aa5c385de6686781dd5ce
noble-varghese <noblekvarghese96@gmail.com>
2023-09-07 15:34:58 +0530

    fix: update the examples

--- examples/demo.py
+++ examples/demo.py
@@ -1,24 +1,32 @@
 import portkey
 from portkey import Config, LLMBase, Message, Params
 
-portkey.api_key = ""
-portkey.base_url = ""
+# portkey.api_key = ""
+# portkey.base_url = ""
 
 messages = [
     Message(role="system", content="You are a helpful assistant"),
     Message(role="user", content="What can you do?"),
 ]
-# portkey.mode = "loadbalance"
-config = Config(
-    mode="loadbalance",
+portkey.config = Config(
+    mode="fallback",
     llms=[
         LLMBase(
-            provider="openai", model="gpt-3.5-turbo", max_tokens=250, messages=messages,
-            api_key=""),
-        LLMBase(provider="anthropic", model="claude-2", max_tokens=250,),],
-    params=Params(messages=messages),)
+            provider="openai",
+            model="gpt-3.5-turbo",
+            max_tokens=250,
+            messages=messages,
+            api_key="",
+        ),
+        LLMBase(
+            provider="anthropic",
+            model="claude-2",
+            max_tokens=250,
+        ),
+    ],
+    params=Params(messages=messages),
+)
 
-response = portkey.ChatCompletions.create(config=config, stream=True)
-# portkey.Completions.create()
+response = portkey.ChatCompletions.create(stream=True)
 for i in response:
     print(i.choices[0].get("delta", {}).get("content"), end="", flush=True)
