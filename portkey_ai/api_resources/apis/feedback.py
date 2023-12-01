ac4b600eb675a599d9048436fb6ab6188408b7fe
noble-varghese <noblekvarghese96@gmail.com>
2023-12-01 16:43:54 +0530

    fix: adding metadata in feedback routes

--- portkey_ai/api_resources/apis/feedback.py
+++ portkey_ai/api_resources/apis/feedback.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict, Any
 from portkey_ai.api_resources.apis.api_resource import APIResource
 from portkey_ai.api_resources.base_client import APIClient
 from portkey_ai.api_resources.streaming import Stream
@@ -15,8 +15,10 @@ class Feedback(APIResource):
         trace_id: Optional[str] = None,
         value: Optional[int] = None,
         weight: Optional[float] = None,
+        metadata: Optional[Dict[str, Any]] = None
     ) -> None:
-        body = dict(trace_id=trace_id, value=value, weight=weight)
+        body = dict(trace_id=trace_id, value=value,
+                    weight=weight, metadata=metadata)
         return self._post(
             PortkeyApiPaths.FEEDBACK_API,
             body=body,
