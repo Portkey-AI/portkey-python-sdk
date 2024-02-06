8527f9d9a5ab5342557833975ce6c6f003ca4e82
csgulati09 <chandeepsinghgulati@gmail.com>
2024-02-06 19:42:23 +0530

    changes made to support async /complete

--- portkey_ai/api_resources/apis/complete.py
+++ portkey_ai/api_resources/apis/complete.py
@@ -6,8 +6,8 @@ from portkey_ai.api_resources.utils import (
     TextCompletionChunk,
 )
 
-from portkey_ai.api_resources.streaming import Stream
-from portkey_ai.api_resources.apis.api_resource import APIResource
+from portkey_ai.api_resources.streaming import AsyncStream, Stream
+from portkey_ai.api_resources.apis.api_resource import APIResource, AsyncAPIResource
 
 
 class Completion(APIResource):
@@ -82,6 +82,82 @@ class Completion(APIResource):
             params=None,
             cast_to=TextCompletion,
             stream_cls=Stream[TextCompletionChunk],
+            stream=stream,
+            headers={},
+        )
+
+class Completion(AsyncAPIResource):
+    def __init__(self, client: AsyncAPIResource) -> None:
+        super().__init__(client)
+
+    @overload
+    async def create(
+        self,
+        *,
+        prompt: Optional[str] = None,
+        stream: Literal[True],
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        top_k: Optional[int] = None,
+        top_p: Optional[float] = None,
+        **kwargs,
+    ) -> AsyncStream[TextCompletionChunk]:
+        ...
+
+    @overload
+    async def create(
+        self,
+        *,
+        prompt: Optional[str] = None,
+        stream: Literal[False] = False,
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        top_k: Optional[int] = None,
+        top_p: Optional[float] = None,
+        **kwargs,
+    ) -> TextCompletion:
+        ...
+
+    @overload
+    async def create(
+        self,
+        *,
+        prompt: Optional[str] = None,
+        stream: bool = False,
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        top_k: Optional[int] = None,
+        top_p: Optional[float] = None,
+        **kwargs,
+    ) -> Union[TextCompletion, AsyncStream[TextCompletionChunk]]:
+        ...
+
+    async def create(
+        self,
+        *,
+        prompt: Optional[str] = None,
+        stream: bool = False,
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        top_k: Optional[int] = None,
+        top_p: Optional[float] = None,
+        **kwargs,
+    ) -> Union[TextCompletion, AsyncStream[TextCompletionChunk]]:
+        body = dict(
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_k=top_k,
+            top_p=top_p,
+            stream=stream,
+            **kwargs,
+        )
+        return await self._post(
+            PortkeyApiPaths.TEXT_COMPLETE_API,
+            body=body,
+            params=None,
+            cast_to=TextCompletion,
+            stream_cls=AsyncStream[TextCompletionChunk],
             stream=stream,
             headers={},
         )
