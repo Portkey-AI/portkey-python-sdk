a97048fffc20cf2ef80296636ebf4931b41a3a16
csgulati09 <chandeepsinghgulati@gmail.com>
2024-05-31 18:48:44 +0530

    fix: make build issues fixed

--- portkey_ai/api_resources/apis/create_headers.py
+++ portkey_ai/api_resources/apis/create_headers.py
@@ -13,7 +13,7 @@ class CreateHeaders:
         headers = {}
         for k, v in self.kwargs.items():
             # logic for boolean type headers
-            if type(v) == bool:
+            if isinstance(v, bool):
                 v = str(v).lower()
             if k == "mode" and "proxy" not in v:
                 v = f"proxy {v}"
