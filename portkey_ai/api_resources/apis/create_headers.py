f96595e0c5dd9abc757814c6e7fac7621ceb45cb
csgulati09 <chandeepsinghgulati@gmail.com>
2024-05-31 17:27:43 +0530

    fix: none type for debug and cache force refresh

--- portkey_ai/api_resources/apis/create_headers.py
+++ portkey_ai/api_resources/apis/create_headers.py
@@ -30,6 +30,7 @@ class CreateHeaders:
                 # logic for List of str to comma separated string
                 if k == "forward-headers":
                     headers[get_portkey_header(k)] = ",".join(v)
+        print(headers)
         return headers
 
 
