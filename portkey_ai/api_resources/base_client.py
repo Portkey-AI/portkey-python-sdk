4a5016a8dab907d937d7b2b29e1a40cb992ca9ee
Narendranath Gogineni <narenrockstar1@gmail.com>
2024-09-16 14:32:56 +0530

    fix: add header for input audio cost calculation

--- portkey_ai/api_resources/base_client.py
+++ portkey_ai/api_resources/base_client.py
@@ -62,6 +62,7 @@ class APIClient:
         forward_headers: Optional[List[str]] = None,
         openai_project: Optional[str] = None,
         openai_organization: Optional[str] = None,
+        input_audio_length: Optional[int] = None,
         aws_secret_access_key: Optional[str] = None,
         aws_access_key_id: Optional[str] = None,
         aws_session_token: Optional[str] = None,
@@ -91,6 +92,7 @@ class APIClient:
         self.forward_headers = forward_headers
         self.openai_project = openai_project
         self.openai_organization = openai_organization
+        self.input_audio_length = input_audio_length
         self.aws_secret_access_key = aws_secret_access_key
         self.aws_access_key_id = aws_access_key_id
         self.aws_session_token = aws_session_token
@@ -120,6 +122,7 @@ class APIClient:
             forward_headers=forward_headers,
             openai_project=openai_project,
             openai_organization=openai_organization,
+            input_audio_length=input_audio_length,
             aws_secret_access_key=aws_secret_access_key,
             aws_access_key_id=aws_access_key_id,
             aws_session_token=aws_session_token,
@@ -552,6 +555,7 @@ class AsyncAPIClient:
         forward_headers: Optional[List[str]] = None,
         openai_project: Optional[str] = None,
         openai_organization: Optional[str] = None,
+        input_audio_length: Optional[int] = None,
         aws_secret_access_key: Optional[str] = None,
         aws_access_key_id: Optional[str] = None,
         aws_session_token: Optional[str] = None,
@@ -581,6 +585,7 @@ class AsyncAPIClient:
         self.forward_headers = forward_headers
         self.openai_project = openai_project
         self.openai_organization = openai_organization
+        self.input_audio_length = input_audio_length
         self.aws_secret_access_key = aws_secret_access_key
         self.aws_access_key_id = aws_access_key_id
         self.aws_session_token = aws_session_token
@@ -610,6 +615,7 @@ class AsyncAPIClient:
             forward_headers=forward_headers,
             openai_project=openai_project,
             openai_organization=openai_organization,
+            input_audio_length=input_audio_length,
             aws_secret_access_key=aws_secret_access_key,
             aws_access_key_id=aws_access_key_id,
             aws_session_token=aws_session_token,
