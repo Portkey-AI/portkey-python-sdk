cd9648382c28d61183889158bd2acb6bcbab50a7
Narendranath Gogineni <narenrockstar1@gmail.com>
2025-02-13 13:07:59 +0530

    fix: check package installed version

--- portkey_ai/api_resources/instrumentation/__init__.py
+++ portkey_ai/api_resources/instrumentation/__init__.py
@@ -1,4 +1,4 @@
-from importlib import metadata
+from importlib.metadata import version
 from typing import Dict
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
@@ -19,12 +19,20 @@ package_instrumentor_map: Dict[str, BaseInstrumentor] = {
 }
 
 
+def is_package_installed(pkg_name):
+    try:
+        version(pkg_name)
+        return True
+    except:
+        return False
+
+
 def initialize_instrumentation(api_key: str, base_url: str):
     tracer_provider = TracerProvider()
     exporter = PortkeySpanExporter(api_key=api_key, base_url=base_url)
     trace.set_tracer_provider(tracer_provider)
     tracer_provider.add_span_processor(BatchSpanProcessor(exporter))
     for package, instrumentor in package_instrumentor_map.items():
-        if metadata.version(package):
+        if is_package_installed(package):
             instrumentor().instrument()
             print(f"Portkey: {package} Instrumentation initialized")
