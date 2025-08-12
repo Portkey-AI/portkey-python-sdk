d579317bc6f32c027a3a8c204866e7295fb376b3
csgulati09 <chandeepsinghgulati@gmail.com>
2025-08-12 11:57:43 +0530

    feat: steaming for images generate

--- portkey_ai/api_resources/apis/images.py
+++ portkey_ai/api_resources/apis/images.py
@@ -1,10 +1,16 @@
 import json
-from typing import Union, Any
+from typing import Literal, Optional, Union, Any
 import typing
+from portkey_ai._vendor.openai._streaming import AsyncStream, Stream
+from portkey_ai._vendor.openai.types.image_gen_stream_event import ImageGenStreamEvent
+from portkey_ai._vendor.openai.types.images_response import (
+    ImagesResponse as OpenAIImagesResponse,
+)
 from portkey_ai.api_resources.apis.api_resource import APIResource, AsyncAPIResource
 from portkey_ai.api_resources.client import AsyncPortkey, Portkey
 from portkey_ai.api_resources.types.image_type import ImagesResponse
 from ..._vendor.openai._types import NotGiven, NOT_GIVEN
+from typing_extensions import overload
 
 
 class Images(APIResource):
@@ -68,35 +74,118 @@ class Images(APIResource):
 
         return data
 
-    @typing.no_type_check
+    @overload
     def generate(
         self,
         *,
         prompt: str,
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
         model: Union[str, NotGiven] = NOT_GIVEN,
-        n: Union[int, NotGiven] = NOT_GIVEN,
-        quality: Union[str, NotGiven] = NOT_GIVEN,
-        response_format: Union[str, NotGiven] = NOT_GIVEN,
-        size: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        stream: Union[Optional[Literal[False]], NotGiven] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
         user: Union[str, NotGiven] = NOT_GIVEN,
-        style: Union[str, NotGiven] = NOT_GIVEN,
         **kwargs
-    ) -> ImagesResponse:
-        response = self.openai_client.with_raw_response.images.generate(
-            prompt=prompt,
-            model=model,
-            n=n,
-            quality=quality,
-            response_format=response_format,
-            size=size,
-            style=style,
-            user=user,
-            extra_body=kwargs,
-        )
-        data = ImagesResponse(**json.loads(response.text))
-        data._headers = response.headers
+    ) -> OpenAIImagesResponse:
+        ...
 
-        return data
+    @overload
+    def generate(
+        self,
+        *,
+        prompt: str,
+        stream: Literal[True],
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        model: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        user: Union[str, NotGiven] = NOT_GIVEN,
+        **kwargs
+    ) -> Stream[ImageGenStreamEvent]:
+        ...
+
+    @overload
+    def generate(
+        self,
+        *,
+        prompt: str,
+        stream: bool,
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        model: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        user: Union[str, NotGiven] = NOT_GIVEN,
+        **kwargs
+    ) -> Union[OpenAIImagesResponse, Stream[ImageGenStreamEvent]]:
+        ...
+
+    def generate(
+        self,
+        *,
+        prompt: str,
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        model: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        stream: Union[
+            Optional[Union[Literal[False], Literal[True]]], NotGiven
+        ] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        user: Union[str, NotGiven] = NOT_GIVEN,
+        **kwargs
+    ) -> Union[OpenAIImagesResponse, Stream[ImageGenStreamEvent]]:
+        extra_headers = kwargs.pop("extra_headers", None)
+        extra_query = kwargs.pop("extra_query", None)
+        extra_body = kwargs.pop("extra_body", None)
+        timeout = kwargs.pop("timeout", None)
+
+        return self.openai_client.images.generate(  # type: ignore[misc]
+            prompt=prompt,
+            background=background,  # type: ignore[arg-type]
+            model=model,
+            moderation=moderation,  # type: ignore[arg-type]
+            n=n,
+            output_compression=output_compression,
+            output_format=output_format,  # type: ignore[arg-type]
+            partial_images=partial_images,
+            quality=quality,  # type: ignore[arg-type]
+            response_format=response_format,  # type: ignore[arg-type]
+            size=size,  # type: ignore[arg-type]
+            stream=stream,  # type: ignore[arg-type]
+            style=style,  # type: ignore[arg-type]
+            user=user,
+            extra_headers=extra_headers,
+            extra_query=extra_query,
+            extra_body=extra_body,
+            timeout=timeout,
+        )
 
 
 class AsyncImages(AsyncAPIResource):
@@ -158,32 +247,115 @@ class AsyncImages(AsyncAPIResource):
 
         return data
 
-    @typing.no_type_check
+    @overload
     async def generate(
         self,
         *,
         prompt: str,
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
         model: Union[str, NotGiven] = NOT_GIVEN,
-        n: Union[int, NotGiven] = NOT_GIVEN,
-        quality: Union[str, NotGiven] = NOT_GIVEN,
-        response_format: Union[str, NotGiven] = NOT_GIVEN,
-        size: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        stream: Union[Optional[Literal[False]], NotGiven] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
         user: Union[str, NotGiven] = NOT_GIVEN,
-        style: Union[str, NotGiven] = NOT_GIVEN,
         **kwargs
-    ) -> ImagesResponse:
-        response = await self.openai_client.with_raw_response.images.generate(
-            prompt=prompt,
-            model=model,
-            n=n,
-            quality=quality,
-            response_format=response_format,
-            size=size,
-            style=style,
-            user=user,
-            extra_body=kwargs,
-        )
-        data = ImagesResponse(**json.loads(response.text))
-        data._headers = response.headers
+    ) -> OpenAIImagesResponse:
+        ...
 
-        return data
+    @overload
+    async def generate(
+        self,
+        *,
+        prompt: str,
+        stream: Literal[True],
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        model: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        user: Union[str, NotGiven] = NOT_GIVEN,
+        **kwargs
+    ) -> AsyncStream[ImageGenStreamEvent]:
+        ...
+
+    @overload
+    async def generate(
+        self,
+        *,
+        prompt: str,
+        stream: bool,
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        model: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        user: Union[str, NotGiven] = NOT_GIVEN,
+        **kwargs
+    ) -> Union[OpenAIImagesResponse, AsyncStream[ImageGenStreamEvent]]:
+        ...
+
+    async def generate(
+        self,
+        *,
+        prompt: str,
+        background: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        model: Union[str, NotGiven] = NOT_GIVEN,
+        moderation: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        n: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_compression: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        output_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        partial_images: Union[Optional[int], NotGiven] = NOT_GIVEN,
+        quality: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        response_format: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        size: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        stream: Union[
+            Optional[Union[Literal[False], Literal[True]]], NotGiven
+        ] = NOT_GIVEN,
+        style: Union[Optional[str], NotGiven] = NOT_GIVEN,
+        user: Union[str, NotGiven] = NOT_GIVEN,
+        **kwargs
+    ) -> Union[OpenAIImagesResponse, AsyncStream[ImageGenStreamEvent]]:
+        extra_headers = kwargs.pop("extra_headers", None)
+        extra_query = kwargs.pop("extra_query", None)
+        extra_body = kwargs.pop("extra_body", None)
+        timeout = kwargs.pop("timeout", None)
+
+        return await self.openai_client.images.generate(  # type: ignore[misc]
+            prompt=prompt,
+            background=background,  # type: ignore[arg-type]
+            model=model,
+            moderation=moderation,  # type: ignore[arg-type]
+            n=n,
+            output_compression=output_compression,
+            output_format=output_format,  # type: ignore[arg-type]
+            partial_images=partial_images,
+            quality=quality,  # type: ignore[arg-type]
+            response_format=response_format,  # type: ignore[arg-type]
+            size=size,  # type: ignore[arg-type]
+            stream=stream,  # type: ignore[arg-type]
+            style=style,  # type: ignore[arg-type]
+            user=user,
+            extra_headers=extra_headers,
+            extra_query=extra_query,
+            extra_body=extra_body,
+            timeout=timeout,
+        )
