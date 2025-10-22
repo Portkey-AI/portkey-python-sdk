217f6a646044c8e16d98c0349ef5738c607f172d
csgulati09 <chandeepsinghgulati@gmail.com>
2025-10-22 14:53:54 +0530

    fix: integrations models list response type

--- portkey_ai/api_resources/types/integrations_type.py
+++ portkey_ai/api_resources/types/integrations_type.py
@@ -103,33 +103,19 @@ class IntegrationCreateResponse(BaseModel, extra="allow"):
 
 class IntegrationModel(BaseModel, extra="allow"):
     slug: Optional[str] = None
-    pricing_config: Optional[Dict[str, Any]] = None
-    enabled: Optional[bool] = None
-    is_custom: Optional[bool] = None
-    is_finetune: Optional[bool] = None
-    base_model_slug: Optional[str] = None
     created_at: Optional[str] = None
     last_updated_at: Optional[str] = None
-    object: Optional[str] = None
-    _headers: Optional[httpx.Headers] = PrivateAttr()
-
-    def get_headers(self) -> Optional[Dict[str, str]]:
-        return parse_headers(self._headers)
-
-    def __str__(self):
-        return json.dumps(self.dict(), indent=4)
-
-    def __getitem__(self, key):
-        return getattr(self, key, None)
-
-    def get(self, key: str, default: Optional[Any] = None):
-        return getattr(self, key, None) or default
+    enabled: Optional[bool] = None
+    is_custom: Optional[bool] = None
+    pricing_config: Optional[Dict[str, Any]] = None
+    is_finetune: Optional[bool] = None
+    base_model_slug: Optional[str] = None
 
 
 class IntegrationModelsResponse(BaseModel, extra="allow"):
+    models: Optional[List[IntegrationModel]] = None
+    allow_all_models: Optional[bool] = None
     object: Optional[str] = None
-    total: Optional[int] = None
-    data: Optional[List[IntegrationModel]] = None
     _headers: Optional[httpx.Headers] = PrivateAttr()
 
     def get_headers(self) -> Optional[Dict[str, str]]:
